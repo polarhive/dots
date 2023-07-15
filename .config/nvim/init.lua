dedbbc96c00131960cb5d25a6cabbf2a892617d0
Nathan Paul <mail@polarhive.net>
2023-07-16 00:39:08 +0530

    nvim: more tweaks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,19 +5,38 @@ local g = vim.g
 local o = vim.opt
 
 -- tweaks
+o.autoindent = true
 o.clipboard = "unnamedplus"
 o.cursorline = true
-o.hlsearch = true
+o.expandtab = true
+o.hlsearch = false
+o.ignorecase = true
 o.incsearch = true
+o.linebreak = true
 o.number = true
+o.ruler = true
+o.shiftwidth = 4
+o.showmatch = true
 o.smartcase = true
+o.smartindent = true
+o.smarttab = true
+o.softtabstop= 4
 o.swapfile = false
 o.termguicolors = true
+o.textwidth=72
+o.undolevels = 1000
+o.visualbell = true
 o.wildmenu = true
 o.wrap = false
+-- o.spell = true
 
 -- vimscript
 cmd [[
 autocmd BufWritePre * %s/\s\+$//e
 colorscheme lunaperche
 ]]
+
+return require('packer').startup(function(use)
+ use 'wbthomason/packer.nvim'
+end)
+
