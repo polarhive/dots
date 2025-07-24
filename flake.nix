72dbca8e60069a83f319de9e0e3a88bb0a19da75
Nathan Paul <mail@polarhive.net>
2025-07-25 04:21:25 +0530

    mint/brew: add casks

--- flake.nix
+++ flake.nix
@@ -7,31 +7,83 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      environment.systemPackages =
-        [ pkgs.vim
+  outputs = inputs@{ self, nixpkgs, nix-darwin }:
+    let
+      system = "aarch64-darwin";
+      configuration = { pkgs, config, ... }: {
+        nix = {
+          settings = {
+            experimental-features = "nix-command flakes";
+          };
+          optimise.automatic = true;
+        };
+
+        nixpkgs = {
+          config.allowUnfree = true;
+          hostPlatform = system;
+        };
+
+        environment.variables.ZDOTDIR = "/Users/polarhive/.config/zsh";
+        security.pam.services.sudo_local.touchIdAuth = true;
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.primaryUser = "polarhive";
+        system.stateVersion = 6;
+        users.users.polarhive = {
+          home = "/Users/polarhive";
+          shell = pkgs.zsh;
+        };
+
+        environment.systemPackages = with pkgs; [
+          bat
+          eza
+          kitty
+          mkalias
+          neovim
+          obsidian
+          yt-dlp
+          tmux
+          zoxide
         ];
 
-      nix.settings.experimental-features = "nix-command flakes";
+        homebrew = {
+          enable = true;
+          onActivation = {
+            upgrade = true;
+            cleanup = "zap";
+          };
+          taps = [
+            "FelixKratz/formulae"
+            "nikitabobko/tap"
+          ];
+          casks = [
+            "orbstack"
+            "orion"
+            "telegram-desktop"
+            "visual-studio-code"
+            "thunderbird"
+            "aerospace"
+          ];
+          brews = [
+            "batt"
+            "sketchybar"
+          ];
+        };
 
-      # Enable alternative shell support in nix-darwin.
-      programs.fish.enable = true;
-
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      nixpkgs.hostPlatform = "aarch64-darwin";
+        system.defaults = {
+          dock.autohide = true;
+          finder.FXPreferredViewStyle = "clmv";
+          loginwindow.GuestEnabled = false;
+          NSGlobalDomain = {
+            AppleICUForce24HourTime = true;
+            AppleInterfaceStyle = "Dark";
+            KeyRepeat = 4;
+          };
+        };
+      };
+    in {
+      darwinConfigurations."mint" = nix-darwin.lib.darwinSystem {
+        system = system;
+        modules = [ configuration ];
+      };
     };
-  in
-  {
-    # darwin-rebuild build --flake .#mint
-    darwinConfigurations."mint" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
